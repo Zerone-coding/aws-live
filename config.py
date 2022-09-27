44d0a3cdf5793f2c56d3ec7c67d00c819931b12b
Zerone-coding <78889338+Zerone-coding@users.noreply.github.com>
2022-09-27 22:45:29 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-2.cmfilhh3xlf8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "s3-employee-bucket"
+customregion = "us-east-1"
